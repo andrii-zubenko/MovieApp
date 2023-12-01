1ac8cd1448eef02c87870b5de0eddd1f5ec81a3f
Andrii Zubenko <andrei.zubenko@gmail.com>
2023-11-30 22:18:27 -0800

    fix splashcreen theme and install splash creen in MainActivity

--- app/src/main/java/com/andrii/movieapp/MainActivity.kt
+++ app/src/main/java/com/andrii/movieapp/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.andrii.movieapp.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppTheme {
